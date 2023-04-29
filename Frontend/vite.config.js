2696261f7c96fe2126955b259d00374e342db2df
Bablu <bk559722@gmail.com>
2023-04-29 09:17:52 +0530

    commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
+
 // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
@@ -60,7 +61,9 @@ export default ({ mode }) => {
 
   return defineConfig({
     server: {
-      host: true,
+      build: {
+        outDir: 'dist/app',
+      },
 
       proxy: {
 
