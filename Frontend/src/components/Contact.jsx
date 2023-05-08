2e29da46752591e4eb429296541137fb03cb9ea5
Bablu <bk559722@gmail.com>
2023-05-08 17:01:44 +0530

    add

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
 
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "https://bablu-kumar-7272.onrender.com",
         "Access-Control-Allow-Credentials": true,
       },
     });
