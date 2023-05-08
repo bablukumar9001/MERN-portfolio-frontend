f3d67526149f7870c3eb9b529deb037a444c0e8b
Bablu <bk559722@gmail.com>
2023-05-08 16:54:21 +0530

    chnage headers

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
 
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Credentials": true,
       },
     });
