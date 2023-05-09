3b7d3e02266cf5c1c10a00121a0f1e2b8f01f1c3
Bablu <bk559722@gmail.com>
2023-05-09 14:44:04 +0530

    comit

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -37,8 +37,6 @@ const Contact = () => {
 
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
       },
     });
 
